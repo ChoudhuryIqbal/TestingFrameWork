11118b4509d66e18bd52707f3a4146ddff127ede
iqbal <iqbalchoudhury2@gmail.com>
2016-07-11 08:54:27 -0500

    basic

---
+++ FoxNews/src/test/java/news/Search.java
@@ -0,0 +1,14 @@
+package news;
+
+import base.CommonAPI;
+import org.testng.annotations.Test;
+
+/**
+ * Created by a on 7/11/2016.
+ */
+public class Search extends CommonAPI {
+    @Test
+    public void searchNews(){
+        
+    }
+}
